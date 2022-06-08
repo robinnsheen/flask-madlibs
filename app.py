18042aae412ed48bd5b5f19e759af6f83cff6f78
Robin Sheen <robinnsheen@berkeley.edu>
2022-06-08 16:07:19 -0700

    added stories

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/")
+def return_home():
+
+    return render_template("questions.html", story = silly_story.prompts)
+
+@app.get("/story")
+def render_story():
+
+    word_list = {}
+
+    for word in silly_story.prompts:
+        word_list[word] = request.arg(word)
+
+    rendered_story = silly_story.generate(word_list)
+
+    return render_template("story.html", html_story = rendered_story)
