e1482239e01fd9b891fb1576f041270f707a700b
Alex Linden <alextl2004@gmail.com>
2022-06-08 17:57:37 -0700

    final commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
+from stories import excited_story
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -10,25 +11,29 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def return_home():
-
+    """"""
     return render_template("select_menu.html")
 
 @app.get("/questions")
-def return_home():
-
-    story = request.args["stories"]
-    return render_template("questions.html", story = silly_story.prompts)
+def return_questions():
+    """"""
+    if request.args["stories"] == "silly_story":
+        story = silly_story
+    else:
+        story = excited_story
+    return render_template("questions.html", story = story.prompts, story_title=story)
 
 
 @app.get("/results")
 def render_story():
-
+    """"""
     word_list = {}
+    story = story_title
 
-    for word in silly_story.prompts:
+    for word in story.prompts:
         value = request.args[word]
         word_list[word] = value
 
-    rendered_story = silly_story.generate(word_list)
+    rendered_story = story.generate(word_list)
 
     return render_template("story.html", html_story = rendered_story)
